55a8c552cdf0c1e084776769c10848bdfa850dc6
siansoft <siansoft@hotmail.com>
2019-12-03 09:24:46 +0300

    new 3-12 9AM

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express')
 var app = express()
-
-app.get('/about', (req, res) => {
-//   res.send('<h1>Hello world</h1>')
-//   res.send({
-//       name: 'Ali',
-//       like: [
-//           'g',
-//           'b'
-//       ]
-//   })
-res.send('<h1>About Page</h1>')
+app.use(express.static(__dirname +'/public'))
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>')
 })
-app.listen(3000)
+app.get('/json', (req, res) => {
+  res.send({
+      name: 'Ali',
+      like: [
+          'g',
+          'b'
+      ]
+  })
+})
+app.get('/about', (req, res) => {
+
+res.send('<h1>Hi This is About Page</h1>')
+})
+app.get('/bad', (req, res) => {
+    res.send({
+            errorMessage: 'Unable to handle request'
+        })
+    })
+app.listen(3000, () => {
+    console.log('Server is runing up on port 3000')
+})

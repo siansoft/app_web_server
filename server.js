cba574a84f5a6280d46200ada513dce627889360
siansoft <siansoft@hotmail.com>
2019-12-03 19:23:48 +0300

    3-12 7PM

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const hbs = require('hbs')
 var app = express()
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname +'/public'))
+hbs.registerPartials(__dirname + 'views/partials')
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>')
+    //res.send('<h1>Hello world</h1>')
+    res.render('home.hbs', {
+        pagetitle: 'Home Page',
+        welcomemessage: 'Welcome to my web site',
+        currentYear: new Date().getFullYear()
+    })
 })
+
 app.get('/json', (req, res) => {
   res.send({
       name: 'Ali',
@@ -15,9 +22,14 @@ app.get('/json', (req, res) => {
       ]
   })
 })
-app.get('/about', (req, res) => {
 
-res.send('<h1>Hi This is About Page</h1>')
+app.get('/about', (req, res) => {
+// res.send('<h1>Hi This is About Page</h1>')
+res.render('about.hbs', {
+    pagetitle: 'About Page',
+    currentYear: new Date().getFullYear()
+})
+
 })
 app.get('/bad', (req, res) => {
     res.send({

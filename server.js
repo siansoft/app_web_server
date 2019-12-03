928c6111863e0e5ca7468bb71b24af665d9226f7
siansoft <siansoft@hotmail.com>
2019-12-03 13:25:40 +0300

    new

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const hbs = require('hbs')
 var app = express()
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname +'/public'))
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>')

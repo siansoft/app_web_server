9926294da868f3fdc2f2cbd04499c54e983b89d4
siansoft <siansoft@hotmail.com>
2019-12-05 12:31:00 +0300

    new 5-12 12PM

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const hbs = require('hbs')
 var app = express()
-hbs.registerPartials(__dirname + 'views/partials')
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname +'/public'))
 
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello world</h1>')
     res.render('home.hbs', {
         pagetitle: 'Home Page',
         welcomemessage: 'Welcome to my web site',
